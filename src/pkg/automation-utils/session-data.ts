b3781e2906f1947e69d3f53460dc0b764a4bd5af
Camille Louédoc <camille.eyries@gmail.com>
2021-04-05 12:41:50 +0200

    refactor: session data schema

--- src/pkg/automation-utils/session-data.ts
+++ src/pkg/automation-utils/session-data.ts
@@ -1,4 +1,5 @@
 import { Page } from "puppeteer";
+import * as z from "zod";
 import { log } from "../services/log";
 
 async function getCookies(page: Page) {
@@ -28,23 +29,23 @@ async function setLocalStorage(page: Page, localStorage: string) {
 
 // note: we are not implementing sessionStorage because the content is removed each time the tab is closed
 
-/**
- * @returns localStorage/cookie data from the page
- */
+export const SessionData = z.object({
+  localStorage: z.string(),
+  cookies: z.string(),
+});
+export type SessionData = z.infer<typeof SessionData>;
+
 export async function getSessionData(page: Page) {
   log.silly("extracting session data");
   return {
     localStorage: await getLocalStorage(page),
     cookies: await getCookies(page),
-  };
+  } as SessionData;
 }
 
-/**
- * Restore session data to the page.
- */
 export async function setSessionData(
   page: Page,
-  { localStorage, cookies }: { localStorage: string; cookies: string }
+  { localStorage, cookies }: SessionData
 ) {
   log.silly("restoring session data");
   await setCookies(page, cookies);
