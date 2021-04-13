c411efbc6f7afb92e496065f9186dbb1cc762204
Camille Louédoc-Eyries <camille.eyries@gmail.com>
2021-04-13 23:09:04 +0200

    switch to oclif + upgrade

--- src/app/cmd/dev.ts
+++ src/app/cmd/dev.ts
@@ -1,5 +1,17 @@
-export async function main() {
-  // dev here
-}
+import { Command } from "@oclif/command";
 
-main();
+export default class Hello extends Command {
+  static description = "start here ;)";
+
+  static examples = [];
+
+  static flags = {};
+
+  static args = [{ name: "file" }];
+
+  async run() {
+    const { args, flags } = this.parse(Hello);
+
+    console.log(args, flags);
+  }
+}
