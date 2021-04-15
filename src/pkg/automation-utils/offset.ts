7c49d701c6c721fe504236b9fbbc967017ddd344
Camille Louédoc-Eyries <camille.eyries@gmail.com>
2021-04-15 20:31:48 +0200

    update offset.ts

--- src/pkg/automation-utils/offset.ts
+++ src/pkg/automation-utils/offset.ts
@@ -3,19 +3,31 @@ import { ElementHandle, Page } from "puppeteer";
 /**
  * @returns the page's height
  */
-export async function getPageHeight(page: Page) {
+export async function getPageHeight(page: Page): Promise<number> {
   return page.evaluate(() => window.innerHeight);
 }
 
 /**
  * @returns the distance of the given el from the top of the page
  */
-export async function getElOffset(el: ElementHandle<Element>) {
+export async function getElOffsetRelativeToPage(
+  el: ElementHandle<Element>
+): Promise<number> {
   return await el.evaluate((el) => {
-    var bodyRect = document.body.getBoundingClientRect(),
+    const bodyRect = document.body.getBoundingClientRect(),
       elemRect = el.getBoundingClientRect(),
       offset = elemRect.top - bodyRect.top;
     return offset;
+  });
+}
+
+export async function getElOffsetRelativeToWindow(
+  el: ElementHandle<Element>
+): Promise<number> {
+  return await el.evaluate((el) => {
+    const elemRect = el.getBoundingClientRect();
+
+    return elemRect.y;
   });
 }
 
@@ -23,7 +35,7 @@ export async function getElOffset(el: ElementHandle<Element>) {
  * @param sel a scrollable element (defaults to html)
  * @returns the scrollTop of the given element
  */
-export async function getScrollTop(page: Page, sel = "body") {
+export async function getScrollTop(page: Page, sel = "html"): Promise<number> {
   return await page.evaluate(
     (sel: string) => document.querySelector(sel)?.scrollTop || -1,
     sel
